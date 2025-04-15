96df225d030bd6ca4c8e729a2a05419e9e8c64e2
Lightninggabe1 <lightninggabe191@gmail.com>
2025-04-15 18:48:43 +0800

    Update Introduction to JavaScript.js
    5/10

--- Learn JavaScript Basics/Introduction to JavaScript.js
+++ Learn JavaScript Basics/Introduction to JavaScript.js
@@ -23,3 +23,9 @@ console.log('JavaScript');
 console.log(2011);
 console.log('Woohoo! I love to code! #codecademy')
 console.log(20.49)
+
+//5/10: Arithmetic Operators
+console.log(23 + 3.5);
+console.log(2025 - 1969);
+console.log(65 / 240);
+console.log(0.2708 * 100);
