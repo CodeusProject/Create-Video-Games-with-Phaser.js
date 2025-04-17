a8ed5815bc12a9cae2c5d869f4b128bd06d3dcb2
Lightninggabe1 <lightninggabe191@gmail.com>
2025-04-17 22:34:38 +0800

    Update Variables.js
    3/10

--- Learn JavaScript Basics/Learn JavaScript Basics/Variables.js
+++ Learn JavaScript Basics/Learn JavaScript Basics/Variables.js
@@ -5,3 +5,8 @@ var favoriteFood = 'pizza'; //or favouriteFood if you're using British English, 
 var numOfSlices = 8;
 console.log(favoriteFood);
 console.log(numOfSlices);
+
+//3/10: Create a Variable: let
+let changeMe = true;
+changeMe = false;
+console.log(changeMe);
