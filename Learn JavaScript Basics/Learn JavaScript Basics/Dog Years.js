2714c948317e591cd0c62adfc17520a619fac000
Lightninggabe1 <lightninggabe191@gmail.com>
2025-04-25 18:31:42 +0800

    Create Dog Years.js
    Short project guys

---
+++ Learn JavaScript Basics/Learn JavaScript Basics/Dog Years.js
@@ -0,0 +1,12 @@
+//They asked us to write a comment to explain, but the name is self-explainatory
+var myAge = 23;
+var earlyYears = 2;
+earlyYears *= 10.5;
+//laterYears is the number of years remaining after we have counted the first two higher-value years.
+laterYears = myAge - 2;
+//Counting the laterYears as dog years.
+laterYears = laterYears * 4;
+//Total dog age
+myAgeInDogYears = earlyYears + laterYears;
+myName = 'Gabriel'.toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
