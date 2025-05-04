b246cdddeee4dace9b86bd8a5ea54a12ad844103
Lightninggabe1 <lightninggabe191@gmail.com>
2025-05-04 09:57:50 +0800

    Update Conditional Statements.js

--- Learn JavaScript Basics/Learn JavaScript Conditionals/Conditional Statements.js
+++ Learn JavaScript Basics/Learn JavaScript Conditionals/Conditional Statements.js
@@ -6,3 +6,24 @@ sale = false;
 if (sale) {
   console.log('Time to buy!');
 }
+
+//3/11: If...Else Statements
+let sale = true;
+
+sale = false;
+
+if(sale) {
+  console.log('Time to buy!');
+} else {
+  console.log('Time to wait for a sale.')
+}
+
+//4/11: Comparison Operators
+let hungerLevel = 7;
+if (hungerLevel > 7) {
+  console.log('Time to eat!')
+} else {
+  console.log('We can eat later!')
+}
+
+//5/11: 
